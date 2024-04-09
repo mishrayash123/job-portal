a5cbf2003bde7a6a4e8d4a1f31d72529131c590a
SHUBHANGI11gaikwad <shubhangigaikwad730@gmail.com>
2024-04-09 13:41:43 +0530

    ik

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,62 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LoginImage from '../assets/Login Page SVG.svg'
 import GoogleLogo from '../assets/google.svg'
 import AppleLogo from '../assets/apple.svg'
-import './Login.css'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const Login = () => {
-    return (
-        <div className='flex items-center w-full px-10 max-h-[720px] box-border overflow-hidden'>
-            <div className='flex h-[791px] w-1/2 justify-center items-center'>
-                <div className='flex flex-col w-[541px] h-[577px] rounded-3xl justify-center items-center loginCard gap-y-8'>
-                    <div className='flex flex-col gap-y-1 justify-center items-center'>
-                        <p className='font-Inter font-medium text-[32px] leading-[38.73px] text-[#000000]'>Welcome Back!</p>
-                        <p className='font-Inter font-medium text-[15px] leading-[18.15px] text-[#000000]'>Please Enter Your Details</p>
-                    </div>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showClearIcon, setShowClearIcon] = useState(false);
+  const history = useNavigate();
 
-                    <div className='flex flex-col gap-y-5' >
-                        <input className='w-[369px] h-[54px] rounded-2xl border-[1px] border-[#989898] pl-5 font-Inter font-medium text-[16px] leading-[19.36px] outline-[#989898] text-[#5B5B5B]'
-                            type='text'
-                            placeholder='Enter your Email'
-                        />
-                        <button className='px-[153px] py-[14px] w-fit rounded-2xl bg-[#33B249] '>
-                            <p className='font-Inter font-semibold text-[14px] leading-[16.94px] text-[#ffffff]'>
-                                <Link to='/'>
-                                    Continue
-                                </Link>
-                            </p>
-                        </button>
-                    </div>
+  const handleInputChange = (e) => {
+    if (e.target.value) {
+      setShowClearIcon(true);
+    } else {
+      setShowClearIcon(false);
+    }
 
-                    <div className='w-[364px] border-[1px] border-[#989898]'></div>
+    if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    } else if (e.target.name === 'password') {
+      setPassword(e.target.value);
+    }
+  };
 
-                    <div className='flex flex-col gap-y-4'>
-                        <button className='flex w-[369px] items-center h-[54px] rounded-2xl border-[1px] border-[#989898] gap-x-3'>
-                            <img src={GoogleLogo} alt="google logo" className='pl-5' />
-                            <p className='font-Inter font-medium text-[16px] leading-[19.36px] text-[#1A1A1A] '>Continue With Google</p>
-                        </button>
-                        <button className='flex w-[369px] items-center h-[54px] rounded-2xl border-[1px] border-[#989898] gap-x-3'>
-                            <img src={AppleLogo} alt="google logo" className='pl-5' />
-                            <p className='font-Inter font-medium text-[16px] leading-[19.36px] text-[#1A1A1A] '>Continue With Apple</p>
-                        </button>
-                    </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const user = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+  
+    try {
+      const response = await axios.post('/auth/login', user);
+  
+      if (response.data.user) {
+        history('/');
+      } else {
+        alert('Wrong user name');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                    <div className='flex w-fit h-[16px] items-center gap-x-1'>
-                        <p>Don’t have an account?</p>
-                        <Link to='/signup' className='text-[#1E93FF] underline'>
-                            Sign Up
-                        </Link>
-                    </div>
-                </div>
+  return (
+    <div className='flex flex-col justify-center items-center h-screen bg-[#F3F4F6]'>
+      <div className='w-[50%] flex justify-center items-center gap-x-16'>
+        <div className='bg-white rounded-[20px] w-[500px] p-[30px] shadow-md'>
+          <h1 className='font-bold text-[24px] leading-[30px]'>Welcome Back</h1>
+          <p className='font-medium text-[16px] leading-[20px]'>Please sign in to continue</p>
 
-            </div>
-            <div className='flex w-1/2 '>
-                <img src={LoginImage} alt="loginimage" className='w-[791px] object-cover' />
-            </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              className='border-[1px] border-[#B5B5B5] p-[15px] rounded-[20px] w-full mt-[30px]'
+              type="email" name="email" onChange={handleInputChange} placeholder="Email" value={email} required />
+            
+            {showClearIcon && (
+            <input
+              className='border-[1px] border-[#B5B5B5] p-[15px] rounded-[20px] w-full mt-[20px]'
+              type="password" name="password" onChange={handleInputChange} placeholder="Password" value={password} required />
+            
+            )}  
+                
+    
+            <button className='px-[153px] py-[14px] w-fit rounded-2xl bg-[#33B249] mt-[30px]' type="submit">
+              <p className='font-Inter font-semibold text-[14px] leading-[16.94px] text-[#ffffff]'>
+                Continue
+              </p>
+            </button>
+            
+          </form>
+
+          <div className='w-[364px] border-[1px] border-[#989898]'></div>
+
+          <div className='flex flex-col gap-y-4'>
+            <button className='flex w-[369px] items-center h-[54px] rounded-2xl border-[1px] border-[#989898] gap-x-3'>
+              <img src={GoogleLogo} alt="google logo" className='pl-5' />
+              <p className='font-Inter font-medium text-[16px] leading-[19.36px] text-[#1A1A1A] '>Continue With Google</p>
+            </button>
+            <button className='flex w-[369px] items-center h-[54px] rounded-2xl border-[1px] border-[#989898] gap-x-3'>
+              <img src={AppleLogo} alt="apple logo" className='pl-5' />
+              <p className='font-Inter font-medium text-[16px] leading-[19.36px] text-[#1A1A1A] '>Continue With Apple</p>
+            </button>
+          </div>
+
+          <p className='mt-[40px] font-medium text-[16px] leading-[20px]'>By signing in, you agree to the <span className='text-[#33B249]'>Terms of Service</span> and <span className='text-[#33B249]'>Privacy Policy</span></p>
+
+          <p className='mt-[40px] font-medium text-[16px] leading-[20px]'>New to Project Flare? <Link to="/signup" className='text-[#33B249]'>Create an account</Link></p>
         </div>
+        <img src={LoginImage} alt="login" className='h-[450px] rounded-[20px] object-cover object-center' />
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Login
+export default Login;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignUpLogo from '../assets/SignUpPage SVG.svg'
 import './Login.css'
 import { Link } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom';
+import UserService from '../services/UserService';
 const SignUp = () => {
+    const [formData, setFormData] = useState({
+        fullname: '',
+        username: '',
+        email: '',
+        password: '',
+        Conditions: '',
+        Type: '',
+      });
+    
+      const history = useNavigate();
+    
+      const { fullname, username, email, password, Conditions, Type } = formData;
+    
+      const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const { data } = await fetch('http://localhost:3000/auth/register', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(formData),
+        }).then((res) => res.json());
+    
+        if (data.sessionToken) {
+          localStorage.setItem('sessionToken', data.sessionToken);
+          history('/dashboard');
+        }
+      };
+    
+  
+
     return (
         <div className='flex justify-center items-center overflow-y-hidden gap-x-10 max-h-[720px]'>
             <div className='flex flex-col w-1/2 gap-3 justify-center items-center pt-20'>
@@ -17,48 +52,77 @@ const SignUp = () => {
 
                 <img src={SignUpLogo} alt="signupLogo" className='w-fit' />
             </div>
+            <form onSubmit={handleSubmit}>
             <div className='flex flex-col justify-center items-center w-1/2 gap-y-4'>
                 <div className='w-[541px] h-[691px] rounded-3xl loginCard flex flex-col justify-center items-center gap-6'>
                     <p className='font-Inter font-medium text-[32px] leading-[38.73px] text-[#000000] pb-5'>Fill in your details</p>
 
                     <div className='flex flex-col gap-x-3 gap-y-3 pb-4'>
-                        <input
+                    <input
                             className='w-[360px] h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
                             type="text"
-                            placeholder='Full Name'
+              name="fullname"
+              id="fullname"
+              value={fullname}
+              onChange={handleChange}
+              placeholder='Full Name'
+              required
+
+                          
                         />
                         <input
-                            className='w-[360px] h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
+                            className='w-full h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
                             type="text"
+                            name="username"
+                            id="username"
+                            value={username}
+                            onChange={handleChange}
+                            required
                             placeholder='User Name'
                         />
                         <input
-                            className='w-[360px] h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
+                            className='w-full h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
                             type="email"
-                            placeholder='Email'
+              name="email"
+              id="email"
+              value={email}
+              onChange={handleChange}
+              required
+              placeholder='Email'
                         />
                         <input
-                            className='w-[360px] h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
+                            className='w-full h-[54px] font-Inter font-medium text-[16px] leading-[19.36px] text-[#7D7D7D] outline-none border-b-[1px] border-[#989898] pt-6'
                             type="password"
-                            placeholder='Password'
+              name="password"
+              id="password"
+              value={password}
+              onChange={handleChange}
+              required
+              placeholder='Password'
                         />
                     </div>
 
                     <div className='flex gap-x-6 items-center w-[360px] pb-2'>
                         <div className='flex gap-x-1'>
                             <input
-                                type="radio"
-                                name="accountType"
-                                value='Candidate'
-                                id="accountType" />
+                            type="radio"
+              name="accountType"
+              id="accountType"
+              value={Conditions}
+              onChange={handleChange}
+              required
+
+                                />
                             <label htmlFor="accountType">Candidate</label>
                         </div>
                         <div className='flex gap-x-1'>
                             <input
                                 type="radio"
-                                name="accountType"
-                                value='Recruiter'
-                                id="accountType" />
+              name="Type"
+              id="Type"
+              value={Type}
+              onChange={handleChange}
+              required />
                             <label htmlFor="accountType">Recruiter</label>
                         </div>
 
@@ -75,7 +139,7 @@ const SignUp = () => {
 
                     <div className='flex justify-between w-[360px] items-center'>
                         <div className='px-[44px] py-[14px] bg-[#33B249]'>
-                            <p className='text-[#ffffff] font-Inter text-[14px] leading-[16.94px] font-semibold'>Register Now</p>
+                            <button className='text-[#ffffff] font-Inter text-[14px] leading-[16.94px] font-semibold'>Register Now</button>
                         </div>
                         <p className='text-[#1E93FF] font-Inter font-semibold text-[13px] leading-[15.73px] underline'>
                             <Link to='/login'>
@@ -85,9 +149,11 @@ const SignUp = () => {
                         </p>
                     </div>
 
-
+                        
                 </div>
+                
             </div>
+             </form>
         </div>
     )
 }
